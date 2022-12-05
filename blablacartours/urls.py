02944e8119dd4a3c3ad5ffbabd9c523079165cc9
Badara <badou017@gmail.com>
2022-12-05 21:15:34 +0000

    Initial Commit

---
+++ blablacartours/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homepage),
+]
+
+urlpatterns += staticfiles_urlpatterns()
